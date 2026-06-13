e055de3488816eb8fdf6f7f4d5d30ded7bf31ed0
JaviLF1 <145053537+JaviLF1@users.noreply.github.com>
2026-06-12 23:12:14 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <chrono>
+#include<Windows.h>
+//#include <date/date.h>
+
+#include "Task.h"
+
+int main()
+{
+
+    
+    Task tsk(42, 3, 2026, "Title", "MainText");
+    /*
+    Task tsk1(2, 3, 2026, "Second Title", "Second MainText");
+
+    Task tsk2(3, 4, 2026, "Title", "MainText");
+    
+    
+    std::vector<Task> all = Task::loadAllEntries();
+    for (auto& entry : all)
+        std::cout << entry.getDay() << "/" << entry.getMonth() << "/" << entry.getYear()
+        << " - " << entry.getTitleTask() << " - " << entry.getDescriptionTask() << "\n";
+
+
+    all[1].getTitleTask();
+    
+    for (int i = 0; i++; i < 3) {
+
+        std::cout << all[i].getTitleTask()<<"\n";
+
+    }*/
+    
+
+    
+
+
+
+
+    return 0;
+} 
